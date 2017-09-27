705c6d1a5f9bbd6e4444966cfb6293b2d3b704be
Chris Barnes <cjbarnes312@gmail.com>
2017-09-26 21:08:17 -0400

    Api searches appear in network.progress

---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_KEY = 'c22a3a4b4fdcd7d6c133d89090dcfac5';
+/*using es6 template strings instead. kind of cleaner i guess
+const URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=s' + API_KEY; */
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// instead of using 'FETCH_WEATHER' as a string in type made it a 
+// variable so action types stay consistent between action creators and reducers 
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	// Static us country code 
+	//api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+	const url = `${ROOT_URL}&q=${city},us`
+	const request = axios.get(url);
+
+	return{
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
---
+++ src/containers/search_bar.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather} from '../actions/index';
+
+class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {term: ''};
+		// take existing function(onInputchange) bind it to this. place the existing function with it
+		// passing a callback around and it has a referance to this you need to bind the context
+		this.onInputChange = this.onInputChange.bind(this);
+		//same as above.  callback to dom element needs context bound. otherwise props = null error in console.
+		this.onFormSubmit = this.onFormSubmit.bind(this);
+	}
+
+	onInputChange(event) {
+		this.setState({term: event.target.value})
+	}
+
+	onFormSubmit(event) {
+		event.preventDefault();
+		// term was already defined as event.target.value which is the city the users enters
+		this.props.fetchWeather(this.state.term);
+		//clearing search input
+		this.setState({ term: "" });
+	}
+
+	render () {
+		return (
+			<form onSubmit={this.onFormSubmit} className="input-group">
+				<input 
+					placeholder="Get a five day forecast in your favorite cities"
+					className="form-control"
+					value={this.state.term}
+					onChange={this.onInputChange}
+				/>
+				<span className="input-group-button">
+					<button type="submit" className="btn btn-secondary">Submit</button>
+				</span>
+			</form>
+		);
+	}
+}
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// null there for when a function is passed it always goes in to the second argument there since theres is no state there therefore it goes straight to dispatch
+export default connect(null, mapDispatchToProps)(SearchBar);
+
+
+
+
+
+
+
