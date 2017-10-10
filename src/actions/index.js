9244e8e82bb8d64bd9fef08c11074d4e947f6a69
Chris Barnes <cjbarnes312@gmail.com>
2017-10-10 17:55:03 -0400

    fixed api link for heroku

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const API_KEY = 'c22a3a4b4fdcd7d6c133d89090dcfac5';
 /*using es6 template strings instead. kind of cleaner i guess
 const URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=s' + API_KEY; */
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 // instead of using 'FETCH_WEATHER' as a string in type made it a 
 // variable so action types stay consistent between action creators and reducers 
